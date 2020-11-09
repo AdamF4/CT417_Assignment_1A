3750565f1ec5dbb68ecfc250c7f3b64e9bab98f8
AdamF4 <56345036+AdamF4@users.noreply.github.com>
2020-11-09 14:13:20 +0000

    Added constructor and private variables for Student.java

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,19 @@
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+
 public class Student {
+    private String name;
+    private int age;
+    private LocalDate DOB;
+    private int ID;
+    private String username;
+    private ArrayList<Course> courses;
+
+    Student(String name, LocalDate DOB, int ID, ArrayList<Course> courses){
+        this.name=name;
+        this.DOB=DOB;
+        this.ID=ID;
+        this.courses=courses;
+    }
 }
